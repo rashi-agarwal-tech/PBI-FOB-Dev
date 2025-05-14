ef245a71822c6bff825dfa97279a7892314533e1
Martin Kingston <martin.kingston@kingstonbi.com>
2025-05-14 16:43:16 +0100

    ci: add commit and push pbi pipeline changes [skip ci]

--- pbi_tools/cli.py
+++ pbi_tools/cli.py
@@ -29,7 +29,7 @@ app = typer.Typer()
 console = Console()
 
 
-# Added a comment to generat a change for a pipeline test.
+# Added a comment to generate a change for a pipeline test.
 def chunks(data, size=10000):
     it = iter(data)
     for _ in range(0, len(data), size):
